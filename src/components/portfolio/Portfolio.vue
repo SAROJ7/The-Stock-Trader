9a6562dce06108dd17498fd271d89a81a8c29825
Saroj Shrestha <sarojsht377@gmail.com>
2020-06-13 12:47:47 +0545

    Portfolio and Stocks

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-stock v-for="stock in stocks" :key="stock" :stock="stock"></app-stock>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+import Stock from './Stock.vue';
+
+export default {
+    computed:{
+        ...mapGetters({
+            stocks: 'stockPortfolio',
+            funds:'funds'
+        })
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: 'Toshiba', price : 200},
+    {id: 2, name: 'Samsung', price : 250},
+    {id: 3, name: 'Cello', price : 8},
+    {id: 4, name: 'Dew', price : 25}
+
+]
